0453b3c9d5d28a70717f693d2df8240abd33750b
KyrinZero <bayajitmohammed@gmail.com>
2021-01-05 22:34:13 +0800

    Minor Css fixess

--- frontend/src/components/utilities/index.js
+++
@@ -1,9 +0,0 @@
-import {
-  addProductSchema,
-  signInSchema,
-  signUpSchema,
-} from "./authentication_schema";
-import axios from "./axios";
-import generatePDF from "./reportGenerator";
-
-export { addProductSchema, signInSchema, signUpSchema, axios, generatePDF };
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const productRoute = require("./routes/product");
+const historyRoute = require("./routes/history");
+const userRoute = require("./routes/user");
+
+app.use("/product", productRoute);
+app.use("/history", historyRoute);
+app.use("/user", userRoute);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
